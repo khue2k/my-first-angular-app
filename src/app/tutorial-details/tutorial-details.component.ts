fef141002257b48eff19f1b3c4e34d7bfa4fc6e3
khue29 <duongxuankhue@vnpt.vn>
2025-02-10 09:56:24 +0700

    init component

---
+++ src/app/tutorial-details/tutorial-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-details.component.html',
+  styleUrl: './tutorial-details.component.css'
+})
+export class TutorialDetailsComponent {
+
+}
