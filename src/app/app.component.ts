9c433eb90e5683de22724b65e728d79e14d54205
khue29 <duongxuankhue@vnpt.vn>
2025-02-15 18:18:14 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
-import { Product } from './common/Product';
-import { ProductListComponent } from "./components/product-list/product-list.component";
-import { ProductDetailComponent } from "./components/product-detail/product-detail.component";
+import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductListComponent, ProductDetailComponent, RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Sửa lỗi "styleUrl" thành "styleUrls"
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {ProductDetailComponent} from "./components/product-detail/product-detail.component";
 import {ProductListComponent} from "./components/product-list/product-list.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {TutorialsListComponent} from "./tutorials-list/tutorials-list.component";
+import {TutorialDetailsComponent} from "./tutorial-details/tutorial-details.component";
+import {AddTutorialComponent} from "./add-tutorial/add-tutorial.component";
 
 export const routes: Routes = [
-  { path: 'product-detail', component: ProductDetailComponent },
-  { path: 'product-list', component: ProductListComponent },
-  { path: '**', component: PageNotFoundComponent },
+  {path: 'product-detail', component: ProductDetailComponent},
+  {path: 'product-list', component: ProductListComponent},
+  {path: '**', component: PageNotFoundComponent},
+  {path: 'tutorials', component: TutorialsListComponent},
+  {path: ' tutorials/:id', component: TutorialDetailsComponent},
+  {path: 'add', component: AddTutorialComponent}
 ];
 
